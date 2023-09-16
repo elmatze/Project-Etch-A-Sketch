f2ecce1f867727a912b2c62fa0f1dad4740fd113
Matthias <wurster.mb@t-online.de>
2023-09-16 17:48:04 +0200

    Deleted the CSS:hover-classes again. Instead added an Event to each div to add a new class if mouse entered (stays there permanently for now)

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const father = document.querySelector(".father");
 
 for (i = 0; i < 16 * 16; i++) {
     const div = document.createElement("div");
+    div.addEventListener("mouseenter", () =>
+        div.setAttribute("style", "background-color: gray")
+    );
     father.appendChild(div);
 }
 
-    
+
 
 
