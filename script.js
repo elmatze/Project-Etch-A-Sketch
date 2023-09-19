63ea7ce941bf7c4e9481d556b46c358efd923692
Matthias <wurster.mb@t-online.de>
2023-09-19 14:55:55 +0200

    Trying to make a choosable grid size, not working at all lol

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 
+const newGrid = document.querySelector(".newGrid")
 const father = document.querySelector(".father");
 
-for (i = 0; i < 16 * 16; i++) {
-    const div = document.createElement("div");
-    div.addEventListener("mouseenter", () =>
-        div.setAttribute("style", "background-color: gray")
-    );
-    father.appendChild(div);
-}
+const div = document.createElement("div");
 
+createGrid(16 * 16);
+
+newGrid.addEventListener("click", function getGridSizeLength() {
+    // delete the previous grid - for now just the new divs are added to the existing ones
+    let gridSideLength = prompt("How long should the side of the new square grid be?");
+    let gridSize = gridSideLength * gridSideLength;
+    console.log(gridSize);
+    createGrid(gridSize);
+} )
+
+function createGrid(gridSize) {
+    if (gridSize == 256) {
+        for (i = 0; i < gridSize; i++) {
+            div.addEventListener("mouseenter", () =>
+                div.setAttribute("style", "background-color: gray")
+            );
+            father.appendChild(div);
+        };
+    }
+    else {
+        div.remove();
+        for (i = 0; i < gridSize; i++) {
+            div.addEventListener("mouseenter", () =>
+                div.setAttribute("style", "background-color: gray")
+            );
+            father.appendChild(div);
+        };
+    }
+};
 
 
 
