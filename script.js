6670992f6e55e5ecb5bb50d6e0c1daffde21cfa8
Matthias <wurster.mb@t-online.de>
2023-09-23 18:33:14 +0200

    JS: Implemented the choosable grid size with a slider
    This works pretty fine as the pixels (min 9 - max 2500) only take the available place to fit in the father-div.
    Next problem to solve: The correct appearance of the default grid (16*16) and that the custom grids will also be deleted correctly (they kinda stack on top of each other - possibility to crash

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-
 const father = document.querySelector(".father");
+const slider = document.querySelector(".slider");
+const statevalue = document.getElementById("statevalue");
 
-for (i = 0; i < 16 * 16; i++) {
+statevalue.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  if (typeof variable !== 'undefined') {
+    father.removeChild(div) // problematic but maybe the correct way
+  } 
+  output.innerHTML = this.value;
+  gridSize = this.value * this.value; // works!
+  reciprocal = 1 / this.value
+  divSize = reciprocal.toLocaleString("en", {style: "percent", minimumFractionDigits: 3})
+  console.log(divSize)
+  for (i = 0; i < gridSize; i++) {
     const div = document.createElement("div");
-    div.addEventListener("mouseenter", () =>
-        div.setAttribute("style", "background-color: gray")
-    );
+    div.setAttribute("style", `height: ${divSize}`);
+    div.style.width = divSize;
+
+    div.addEventListener("mouseenter", 
+      () => div.setAttribute("style", "background-color: gray",));
+    div.addEventListener("mouseenter",
+      () => div.style.height = divSize);
+    div.addEventListener("mouseenter",
+      () => div.style.width = divSize);
     father.appendChild(div);
+  }
 }
+
 
 
 
