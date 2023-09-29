2c5303d4ca8a925ecfb2a3a0703a6009ac152dd5
Matthias <wurster.mb@t-online.de>
2023-09-29 21:18:04 +0200

    Finally got the substitution of the newer grids correct
    I added a new function for the default (16*16) grid and as soon as I moved the slider my oninput function calculated the fitting values and overwrote the existing grid in line 16, so that there is no problem with the stack overflowing when a new (customised) grid is created. I also added a button in my HTML file, but I actually had no need for it right now.

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const father = document.querySelector(".father");
 const slider = document.querySelector(".slider");
 const statevalue = document.getElementById("statevalue");
+const refreshBtn = document.querySelector(".refresh");
 
 statevalue.innerHTML = slider.value; // Display the -default- (16) slider value
 
+createDefaultGrid()
+
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  if (typeof variable !== 'undefined') {
-    father.removeChild(div) // problematic but maybe the correct way
-  } 
   statevalue.innerHTML = this.value; // displays the current slider value
-  gridSize = this.value * this.value; // works!
+  gridSize = this.value * this.value;
   reciprocal = 1 / this.value
   divSize = reciprocal.toLocaleString("en", {style: "percent", minimumFractionDigits: 3})
-  console.log(divSize)
+  father.innerHTML = "";
+  createGrid()
+}
+  
+function createDefaultGrid() {
+  for (i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("style", `height: 1/16`);
+    div.style.width = 1/16;
+
+    div.addEventListener("mouseenter", 
+      () => div.setAttribute("style", "background-color: gray",));
+    div.addEventListener("mouseenter",
+      () => div.style.height = 1/16);
+    div.addEventListener("mouseenter",
+      () => div.style.width = 1/16);
+    father.appendChild(div)
+  }
+}
+
+function createGrid() {
   for (i = 0; i < gridSize; i++) {
     const div = document.createElement("div");
     div.setAttribute("style", `height: ${divSize}`);
@@ -28,8 +48,3 @@ slider.oninput = function() {
     father.appendChild(div);
   }
 }
-
-
-
-
-
